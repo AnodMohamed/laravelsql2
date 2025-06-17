d9fc292437966139555aa915f53a92e5fb3c2369
unknown <anodmohamed2020@gmail.com>
2025-06-17 10:32:55 +0200

    Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Container\Attributes\Tag;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
-    
+    public function auther() //auther_id
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    protected $with = ['profile'];
     /**
      * The attributes that are mass assignable.
      *
@@ -44,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
     }
 }
